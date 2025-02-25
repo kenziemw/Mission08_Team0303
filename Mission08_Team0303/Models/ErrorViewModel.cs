866f84a81adb8e1258ef69083d461da06be0b455
westonrwatson <westonrwatson@gmail.com>
2025-02-24 21:52:18 -0700

    first

---
+++ Mission08_Team0303/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team0303.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
